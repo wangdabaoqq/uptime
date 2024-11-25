96f72895d4931c99be8f9e5cc8c2a874236c8548
宝宝宝 <wangdabao@js.cool>
2024-11-25 09:47:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2671488-fb491a80091166a48bc66b17',
+    'ur2743301-954072b278d42d76d7725178',
     //'m797638073-924f5efed86446cce1c95ae1', // 周润发的博客 API key
     //'m797713213-1c83620d56db599960ad0244', // 周润发的公益订阅 API key
     //'m797713331-835664f191b589688f122e00', // 周润发的公益订阅器 API key
@@ -22,7 +22,7 @@ window.Config = {
   Navi: [
     {
       text: '主页',
-      url: 'https://uptime.001315.xyz/'
+      url: 'https://skin.940703.xyz/'
     },    
     {
       text: 'GitHub',
